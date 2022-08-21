a1bd694b78b314a7e07cf3442b7b25c90b6c29e4
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2022-08-21 12:04:37 -0400

    Addded logout button in Notebook

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,13 +1,16 @@
-// import React, { useState, useRef } from 'react'
-// import { Card } from "react-bootstrap"
+
 import { Button, Card } from "semantic-ui-react"
 import 'semantic-ui-css/semantic.min.css'
 import ref from "../firebase/Storage"
 import realtimeDB from '../firebase/realtimeDatabase';
 
+import firebase from "../firebase/firebase"
+import firestore from "../firebase/firestore"
+import db from "../firebase/firestore"
+
 import React, { useRef, useState, useEffect } from "react"
 // import { Form, Button, Card, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
+import { useAuth, logout } from "../contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
 
 
@@ -19,18 +22,21 @@ function Notebook() {
   const firstNameRef = useRef()
   const lastNameRef = useRef()
   const emailRef = useRef()
-  const {currentUser, updatePassword, updateEmail} = useAuth()
+  const {currentUser, updatePassword, updateEmail, logout} = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
+  const [info , setInfo] = useState([]);
+
   const sendPost = (e) => {
     e.preventDefault()
     // TODO: Use realtime database instead as storage is only for media type of bjects.
-    alert("post: "+inputRef.current.value)
-    realtimeDB.ref("user 2").set({
+    // alert("post: "+inputRef.current.value)
+    realtimeDB.ref("dataName").set({
       post: inputRef.current.value
     }).catch(alert)
+
   }
   
   const handleChange = (e) => {
@@ -38,6 +44,39 @@ function Notebook() {
     console.log("post: "+inputRef.current.value)
   }
 
+  // Start the fetch operation as soon as
+  // the page loads
+  window.addEventListener('load', () => {
+    Fetchdata();
+  });
+
+  // Fetch the required data using the get() method
+  const Fetchdata = ()=>{
+    db.collection("Users").get().then((querySnapshot) => {
+         
+        // Loop through the data and store
+        // it in array to display
+        querySnapshot.forEach(element => {
+            var data = element.data();
+            setInfo(arr => [...arr , data]);
+              
+        });
+    })
+  }
+
+  async function logginout () {
+
+    setError("")
+
+    try{
+      navigate("/login")
+      await logout()
+      navigate("/login")
+      // alert("Email: "+currentUser.email+" logged out")
+    } catch {
+      setError("failed to log out")
+    }
+  }
   return (
     <div className = "ui centered grid">
       <div className="computer only row">
@@ -78,6 +117,15 @@ function Notebook() {
           </Button>
         {/* </div> */}
       </div>
+      <Button 
+            basic name = "logout"
+              color = "blue"
+              className = "ui incon basic button"
+              type = "submit"
+              size = "big"
+              onClick = { logginout }>  
+              <i className = "logout icon"></i>
+          </Button>
     </div>
   )
 }
