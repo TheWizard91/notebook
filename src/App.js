38b8da2e75bbd0393c42e847bc06b6b1c6b10dab
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2022-08-27 10:56:33 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -23,22 +23,8 @@ class App extends Component {
                 <Router>
                   <AuthProvider>
                     <Routes>
-                      {/* <Route path="/" exact={true} element={<LogIn />} />
-                      <Route path="/login" exact={true} element={<LogIn />} /> */}
-                      {/* <PrivateRoute exact path="/" element={<PrivateRoute />} /> */}
-                      {/* <Route exact path="/dasboard" element={<Dashboard />} /> */}
-                      {/* <Route exact path='/' element={<PrivateRoute/>} >
-                        <Route exact path='/' element={<LogIn/>}/>
-                      </Route> */}
-                      {/* <Route path='/' element={<PrivateRouteTwo/>} >
-                        <Route path='/update-profile' element={<UpdateProfile/>}/>
-                      </Route> */}
-                      {/* <Route path="/signup" element={<SignUp />} />
-                      <Route path="/forgot-password" element={<ForgotPassword />} />
-                      <Route path="/" element = { <Notebook /> } /> */}
-                      {/* <PrivateRoute exact path="/" element={<PrivateRoute />} /> */}
-
-                      <Route exact path="/" element={<Dashboard />} />
+                      {/* <Route exact path="/dashboard" element={<Dashboard />} /> */}
+                      <Route exact path="/dashboard" element={<Dashboard />} />
                       <Route exact path='/' element={<PrivateRoute/>} >
                         <Route exact path='/' element={<LogIn/>}/>
                       </Route>
@@ -49,6 +35,7 @@ class App extends Component {
                       <Route path="/login" element={ <LogIn /> } />
                       <Route path="/forgot-password" element={ <ForgotPassword /> } />
                       <Route path="/notebook" element = { <Notebook /> } />
+                    </Routes>
                   </AuthProvider>
                 </Router>
             </div>
