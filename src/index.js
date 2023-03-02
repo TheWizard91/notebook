ca0ae1bfc26b8b3159d1286317d62b36dc7b8018
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2023-03-02 06:27:40 -0500

    fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
-
 ReactDOM.render(
   <React.StrictMode>
       <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
