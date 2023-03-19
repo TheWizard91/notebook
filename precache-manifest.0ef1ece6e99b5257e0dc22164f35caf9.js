3d1e0e0f04abef79353bc387a6d1ee3aa4a87d37
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2023-03-19 16:59:24 -0400

    Updates

---
+++ precache-manifest.0ef1ece6e99b5257e0dc22164f35caf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c506b2c24ffb00e23a50",
+    "url": "/notebook/static/css/main.a2b4f71a.chunk.css"
+  },
+  {
+    "revision": "c506b2c24ffb00e23a50",
+    "url": "/notebook/static/js/main.1933e7a6.chunk.js"
+  },
+  {
+    "revision": "630a455f09d96863fd89",
+    "url": "/notebook/static/js/runtime~main.0c203988.js"
+  },
+  {
+    "revision": "e022649e5993a00bad85",
+    "url": "/notebook/static/js/2.6aaf4159.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notebook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "01a502b6ab1c3593a38dff9fbc997f89",
+    "url": "/notebook/index.html"
+  }
+];
