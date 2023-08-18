1ea4d210bfe845d048abce91e6e09d1319acea4e
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2023-08-18 19:26:24 -0400

    fixed the start app button

--- src/components/StartAppNav.js
+++ src/components/StartAppNav.js
@@ -16,15 +16,15 @@ function StartAppNav() {
 
     }
     return (
-        // <div>
         <header id="navHeader">
             <div className="left">
                 <h3 id="name">
+                <Link to = "/login" element = { <LogIn/> }>Start The App</Link>
                     {/* <Link 
                         to="/login" 
                         element={<LogIn/>}
                         onClick={toggleVisibility}>Emmanuel K. Agyapong</Link> */}
-                    <Transition 
+                    {/* <Transition 
                         visible={visible} 
                         animation="scale" 
                         duiation={500}>
@@ -32,31 +32,13 @@ function StartAppNav() {
                             content={visible ? 'Hide' : 'Show'}
                             onClick={toggleVisibility}>
                             <Link 
-                                to="/login" 
-                                element={<LogIn/>}
-                                onClick={toggleVisibility}>Start The App</Link>
+                                to="/login"
+                                element={<LogIn/>}>Start The App</Link>
                         </ Button>
-                    </Transition>
-                    {/* <Divider hiden/> */}
-                    {/* <Transition
-                        visible={visible} 
-                        animation="scale" 
-                        duiation={500}>
-                        <Image 
-                            size="small"
-                            src='https://react.semantic-ui.com/images/leaves/1.png'/>
                     </Transition> */}
                 </h3>
             </div>
-            {/* <nav className="menu">
-                <ul className="navbar ml-auto">
-                    <li>
-                        <Link to="" className="nav-link">My work</Link>
-                    </li>
-                </ul>
-            </nav> */}
         </header>
-        // </div>
     )
 }
 export default StartAppNav
